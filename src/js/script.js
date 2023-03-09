432394bea1514da51b451e1521becb067cba9d69
JUNSEOK <kd02109@gmail.com>
2023-03-09 10:40:18 +0900

    [feat]: 모달 팝업창 기능 구현

--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,6 @@
+import { agoraStatesDiscussions } from "./data.js";
 // index.html을 열어서 agoraStatesDiscussions 배열 요소를 확인하세요.
+
 console.log(agoraStatesDiscussions);
 
 // convertToDiscussion은 아고라 스테이츠 데이터를 DOM으로 바꿔줍니다.
@@ -15,8 +17,6 @@ const convertToDiscussion = (obj) => {
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
-
-
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
 };
@@ -32,3 +32,19 @@ const render = (element) => {
 // ul 요소에 agoraStatesDiscussions 배열의 모든 데이터를 화면에 렌더링합니다.
 const ul = document.querySelector("ul.discussions__container");
 render(ul);
+
+//modal 기능 만들기
+const $btnQuestion = document.querySelector(".modalBtn");
+const $btnSubmit = document.querySelector(".btn_submit");
+const $btnClose = document.querySelector(".btn_close");
+const $popupBackground = document.querySelector(".background");
+
+const close = () => {
+  $popupBackground.classList.add("hide");
+};
+const open = () => {
+  $popupBackground.classList.remove("hide");
+};
+
+$btnQuestion.addEventListener("click", open);
+$btnClose.addEventListener("click", close);
